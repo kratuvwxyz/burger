e1f4dbded10a5a4159fda1922fcec0eed3aa0169
desaigner <sai@desaign.me>
2018-07-14 00:46:43 -0500

    adding files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Chicken Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Beef Beat Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('BLT Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Best Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
