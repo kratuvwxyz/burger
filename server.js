78d9d6c2f004c9665890b36031d63ef3848d050d
kratuvwxyz <sai@desaign.me>
2021-03-27 23:00:15 -0500

    main handlebars updated

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-var express = require("express"),
+let express = require("express"),
   PORT = process.env.PORT || 3000,
   app = express();
-app.use(express.static(__dirname + "/public")),
-  app.use(express.urlencoded({ extended: !0 })),
-  app.use(express.json());
-var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({ defaultLayout: "main" })),
-  app.set("view engine", "handlebars");
-var routes = require("./controllers/burgerController.js");
+app.use(express.static(__dirname + "/public")), app.use(express.urlencoded({ extended: !0 })), app.use(express.json());
+let exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" })), app.set("view engine", "handlebars");
+let routes = require("./controllers/burgerController.js");
 app.use(routes), app.listen(PORT, function () {});
